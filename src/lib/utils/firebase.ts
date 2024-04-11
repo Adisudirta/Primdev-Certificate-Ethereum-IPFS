9a2ae8a3ccf2a2cc8f1798f89efeaf00f70222ae
Adisudirta <yanadisudirta@gmail.com>
2024-04-11 15:27:03 +0800

    feat: setup firebase

---
+++ src/lib/utils/firebase.ts
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../config.firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
